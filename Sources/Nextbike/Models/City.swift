f06aed78e1edb706b66623898cca4169cebd7d5e
Hanns Adrian Böhme <hannsadrian@gmail.com>
2024-06-20 13:37:23 +0200

    Extend City and Country models

--- Sources/Nextbike/Models/City.swift
+++ Sources/Nextbike/Models/City.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 public struct City: Decodable {
+    public let id: Int
+    public let lat: Double
+    public let lng: Double
     public let name: String
+    public let placeCount: Int
     public let places: [Place]
+    public let returnToStationsOnly: Bool
+    public let bikeTypes: [String: Int]
+    // TODO: BOUNDS
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "uid"
+        case lat
+        case lng
+        case name
+        case placeCount = "num_places"
+        case places
+        case returnToStationsOnly = "return_to_official_only"
+        case bikeTypes = "bike_types"
+    }
 }
--- Sources/Nextbike/Models/Country.swift
+++ Sources/Nextbike/Models/Country.swift
@@ -8,11 +8,55 @@
 import Foundation
 
 public struct Country: Decodable {
+    public let lat: Double
+    public let lng: Double
     public let name: String
+    public let hotline: String
+    public let domain: String
+    public let language: String
+    public let email: String
+    public let timezone: String
+    public let currency: String
+    public let systemOperatorAddress: String
+    public let country: String
+    public let countryName: String
+    
+    public let terms: URL
+    public let policy: URL
+    public let website: URL
+    public let pricing: URL
+    public let faq: URL
+    
+    public let bookedBikes: Int
+    public let setPointBikes: Int
+    public let availableBikes: Int
+    
+    public let noRegistration: Bool
+    
     public let cities: [City]
-
+    
     private enum CodingKeys: String, CodingKey {
-        case name = "country_name"
+        case lat
+        case lng
+        case name
+        case hotline
+        case domain
+        case language
+        case email
+        case timezone
+        case currency
+        case systemOperatorAddress = "system_operator_address"
+        case country
+        case countryName = "country_name"
+        case terms
+        case policy
+        case website
+        case pricing
+        case faq = "faq_url"
+        case bookedBikes = "booked_bikes"
+        case setPointBikes = "set_point_bikes"
+        case availableBikes = "available_bikes"
+        case noRegistration = "no_registration"
         case cities
     }
 }
